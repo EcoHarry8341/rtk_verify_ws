d9ed0be059c3143009ca78aa9f1ff97dd16c80a0
EcoHarry8341 <875566075@qq.com>
2026-05-28 03:02:22 +0800

    室内调试完成,准备室外建图导航

---
+++ install/rtk_localization/share/rtk_localization/launch/nav2_bringup.launch.py
@@ -0,0 +1,1 @@
+/home/cat/rtk_verify_ws/src/rtk_localization/launch/nav2_bringup.launch.py
--- src/rtk_localization/launch/nav2_bringup.launch.py
+++ src/rtk_localization/launch/nav2_bringup.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
     pkg_rtk = get_package_share_directory('rtk_localization')
     pkg_nav2 = get_package_share_directory('nav2_bringup')
     
-    map_yaml_file = os.path.join(os.path.expanduser('~'), 'rtk_verify_ws', 'maps', 'map052801.yaml')
+    map_yaml_file = os.path.join(os.path.expanduser('~'), 'rtk_verify_ws', 'maps', 'absolute_parking052800.yaml')
     nav2_params_file = os.path.join(pkg_rtk, 'config', 'nav2_params_absolute.yaml')
 
     nav2_launch = IncludeLaunchDescription(
